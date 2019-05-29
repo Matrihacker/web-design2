84fa940e3b6a2d33f3f1827e088a0024f7fbe775
Alexander XU <alexanderxu@appledembp-3.wifi.smc.edu>
2019-05-28 21:14:21 -0700

    push

---
+++ Class15/main.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("burger").display = "block";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("burger").display = "none";
+
+  }
